a6f15d84d46b53d39d4dab4b4380f713dfe1c52c
David Ladowitz <david@ladowitz.com>
2015-05-25 16:13:12 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express     = require('express')
+var superagent  = require('superagent')
+var consolidate = require('consolidate')
+
+var app = express()
+
+app.engine('html', consolidate.handlebars);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'))
+
+var user       = 'azat_co'
+var story_slug = 'kazan'
+
+var api_key  = ""
+var username = ""
+var _token   = ""
+
+app.get('/', function(req, res){
+  superagent.get("http://api.storify.com/v1/stories/" + user + "/" + story_slug)
+  .set({Accept: 'application/json'})
+  .end(function(e, storifyResponse){
+    if (e) return next(e);
+    return res.render('index', storifyResponse.body.content);
+  })
+})
+
+
+app.listen(3001)
